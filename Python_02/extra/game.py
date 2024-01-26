8f9662285c5e321303782f52de899e14316ec45a
Zeka <aminamindhm1212@gmail.com>
2024-01-26 20:06:31 +0330

    i debug my code[array not string]

--- Python_02/extra/game.py
+++ Python_02/extra/game.py
@@ -6,7 +6,7 @@ class Game:
     
     def __repr__(self) -> str:
         np_arr = np.random.randint(0,high=101, size=self.count)
-        result_1 =f'Array     = {np_arr:>2}'
+        result_1 =f'Array     = {np_arr}'
         max_arr = np.max(np_arr)
         result_2 = f'Max value = {max_arr:>4}'
         output = 'WIN' if max_arr > 70 else 'FAIL'
