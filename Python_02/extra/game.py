7e9192fa5cca85e316b0cc31755db0aa3ed96995
Zeka <aminamindhm1212@gmail.com>
2024-01-26 19:53:09 +0330

    I use string trick [:>2]

--- Python_02/extra/game.py
+++ Python_02/extra/game.py
@@ -6,10 +6,10 @@ class Game:
     
     def __repr__(self) -> str:
         np_arr = np.random.randint(0,high=101, size=self.count)
-        result_1 =f'Array     = {np_arr} \n'
+        result_1 =f'Array     = {np_arr:>2}'
         max_arr = np.max(np_arr)
-        result_2 = f'Max value = {max_arr} \n'
+        result_2 = f'Max value = {max_arr:>4}'
         output = 'WIN' if max_arr > 70 else 'FAIL'
-        result_3 = f'Output    = {output} \n'
+        result_3 = f'Output    = {output:>8}'
         
         return result_1 + result_2 + result_3
